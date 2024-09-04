ac609a5d55bb5df5280d860260e3bb48c91bb8f3
이제의 <jeui@ije-uis-MacBook-Air.local>
2024-09-04 09:54:51 +0900

    fix

--- src/pages/Homepage/components/MovieSlide/MovieSlide.jsx
+++ src/pages/Homepage/components/MovieSlide/MovieSlide.jsx
@@ -1,28 +1,9 @@
 import React from "react";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Alert } from "react-bootstrap";
-import MovieCard from "../MovieCard/MovieCard";
-import "./MovieSlide.style.css"
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-    slidesToSlide: 3, // optional, default to 1.
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2, // optional, default to 1.
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1, // optional, default to 1.
-  },
-};
+import MovieSlider from "../../../../common/MovieSlider/MovieSlider";
+import { responsive } from "../../../../constants/responsive";
 
 const MovieSlide = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
@@ -35,31 +16,11 @@ const MovieSlide = () => {
   }
   return (
     <div>
-      <h3>Popular Movies</h3>
-      <Carousel
-        infite={true}
-        centerMode={true}
-        itemClass="movie-slider p-1"
-        containerClass="carousel-container"
+      <MovieSlider
+        title="Popular Movies"
+        movies={data.results}
         responsive={responsive}
-      >
-        {data.results.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </Carousel>
-
-      <h3>Upcoming Movies</h3>
-      <Carousel
-        infite={true}
-        centerMode={true}
-        itemClass="movie-slider p-1"
-        containerClass="carousel-container"
-        responsive={responsive}
-      >
-        {data.results.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </Carousel>
+      />
     </div>
   );
 };
